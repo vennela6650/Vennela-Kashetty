c17a5b79ed567cc9405672cc214a912aa10cb967
vennela6650 <23E51A6650@hitam.org>
2025-07-09 12:19:09 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -4,24 +4,16 @@ import joblib
 # Load model
 model = joblib.load('model/model.pkl')
 
-# Sample patient
+# Example input sample
 sample = pd.DataFrame({
-    'age': [54],
-    'sex': [1],
-    'cp': [0],
-    'trestbps': [130],
-    'chol': [250],
-    'fbs': [0],
-    'restecg': [1],
-    'thalach': [150],
-    'exang': [0],
-    'oldpeak': [1.2],
-    'slope': [1],
-    'ca': [0],
-    'thal': [2]
+    'sepal_length': [5.1],
+    'sepal_width': [3.5],
+    'petal_length': [1.4],
+    'petal_width': [0.2]
 })
 
 # Predict
 prediction = model.predict(sample)[0]
-risk = "At Risk" if prediction == 1 else " Low Risk"
-print(f" Prediction: {prediction} ({risk})")
+print(f"Prediction: {prediction}")
+
+
